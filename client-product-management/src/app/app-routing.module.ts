3bfbade5071a254008b042ff2a2afeee82578ed0
syiham <ihammusoffa@gmail.com>
2023-07-09 12:37:01 +0700

    struktur template dashboard

--- client-product-management/src/app/app-routing.module.ts
+++ client-product-management/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DashboardComponent} from './compnents/admin/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    //admin panel
+    {path: 'dashboard',
+    component: DashboardComponent,
+    },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client-product-management/src/app/app.module.ts
+++ client-product-management/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './compnents/template/sidebar/sidebar.component';
+import { HeaderComponent } from './compnents/template/header/header.component';
+import { FooterComponent } from './compnents/template/footer/footer.component';
+import { DashboardComponent } from './compnents/admin/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
